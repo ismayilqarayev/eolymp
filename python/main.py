c3b8654aadaf7565a5f0c956a2f938c2d7e39e99
Ismayil <149945594+ismayilqarayev@users.noreply.github.com>
2024-10-11 07:54:45 +0400

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,43 +1,18 @@
-from os import name
+class Program:
+    def fun (self):
+        self.__number1 = number1
+        self.__number2 = number2
+
+    def input(self):
+        self.__number1 = int(input("Birinci rəqəmi daxil edin: "))
+        self.__number2 = int(input("İkinci rəqəmi daxil edin: "))
+
+class Runner(Program):
+    def main(self, num1, num2):
+        super().fun(num1, num2)
+        self.__number1 = num1
+        self.__number2 = num2
 
 
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+
         
-    @property
-    def getter_name(self):
-        return self._name
-    
-    @name.setter
-    def setter_name(self, value):
-        if not value:
-            raise ValueError("Name cannot be empty.")
-        self._name = value
-    
-    #----------------------------------------------------------
-    
-    @property
-    def getter_age(self):
-        return self._age
-    
-    @age.setter
-    def setter_age(self, value):
-        if value < 0:
-            raise valueError("Age cannot be negative.")
-        self._age = value
-        
-    def fun_print(self):
-        print(f"Hello, my name is {self.name} and I am {self.age} years old.")
-
-
-
-try:
-    person1 = Person("Tom", 30)
-    person1.fun_print()
-
-    person1.age = -5
-    
-except ValueError as e:
-    print(e)
