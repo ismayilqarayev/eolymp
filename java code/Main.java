9f692bfd9a82d2f7139db4f0a9c89bf453063400
Ismayil <149945594+ismayilqarayev@users.noreply.github.com>
2024-11-19 17:00:22 +0400

    java

---
+++ java code/Main.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Main(){
+    static List<String> adlar = new ArrayList<>();
+    static List<String> soyadlar = new ArrayList<>();
+    static List<String> ata_adi = new ArrayList<>();
+    static List<String> yaslar = new ArrayList<>();
+    static List<String> telefon = new ArrayList<>();
+
+    public static void main(String[] args) {
+        DaxilEdinlenMelumatlar1();
+        DaxilEdinlenMelumatlar2();
+        MelumatlariGoster();
+    }
+
+    public static void DaxilEdinlenMelumatlar1() {
+        
+    }
+
+    public static void DaxilEdinlenMelumatlar2() {
+        
+    }
+
+    public static void MelumatlariGoster() {
+
+    }
+}
