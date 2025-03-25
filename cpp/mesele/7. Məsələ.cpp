d692f01f35c90c74c2efae4e217bfd4079c1981b
Ismayil <149945594+ismayilqarayev@users.noreply.github.com>
2025-03-25 11:00:05 +0400

    update

---
+++ cpp/mesele/7. Məsələ.cpp
@@ -0,0 +1,6 @@
+7. Məsələ:
+Şərt: Bir tam ədəd 𝑎 verilmişdir. Əgər 𝑎 müsbət ədəd olarsa, ekrana “Müsbət ədəd”
+yazdırın, əks halda “Müsbət deyil” yazdırın. Əgər 𝑎 sıfırdırsa, ekrana “Sıfır” yazdırın.
+Giriş: 𝑎 = −5
+Çıxış: “Müsbət deyil”
+
---
+++ cpp/mesele/8. Məsələ.cpp
@@ -0,0 +1,6 @@
+8. Məsələ:
+Şərt: Bir tam ədəd 𝑚 verilmişdir. Əgər 𝑚 5-in tam bölənidirsə, ekrana “5-in böləni”
+yazdırın. Əgər 𝑚 5-ə bölünmürsə, ekrana “5-ə bölünmür” yazdırın.
+Giriş: 𝑚 = 10
+Çıxış: “5-in böləni”
+
