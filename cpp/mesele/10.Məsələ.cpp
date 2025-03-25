fbcb303f34391dad5416b37420bda19285864f96
Ismayil <149945594+ismayilqarayev@users.noreply.github.com>
2025-03-25 11:01:08 +0400

    update

---
+++ cpp/mesele/10.Məsələ.cpp
@@ -0,0 +1,7 @@
+10.Məsələ:
+Şərt: Bir tam ədəd 𝑛 verilmişdir. Əgər 𝑛 10-a tam bölünürsə, ekrana “10-a bölünür”
+yazdırın, əks halda “10-a bölünmür” yazdırın. Əgər 𝑛 sıfırdırsa, ekrana “Sıfır”
+yazdırın.
+Giriş: 𝑛 = 20
+Çıxış: “10-a bölünür”
+
---
+++ cpp/mesele/9. Məsələ.cpp
@@ -0,0 +1,6 @@
+9. Məsələ:
+Şərt: Bir tam ədəd 𝑝 verilmişdir. Əgər 𝑝 müsbət və cüt ədəd olarsa, ekrana “Müsbət
+cüt ədəd” yazdırın. Əgər 𝑝 müsbət və tək ədəd olarsa, ekrana “Müsbət tək ədəd”
+yazdırın. Əgər 𝑝 mənfi ədəd olarsa, ekrana “Mənfi ədəd” yazdırın.
+Giriş: 𝑝 = 6
+Çıxış: “Müsbət cüt ədəd”
