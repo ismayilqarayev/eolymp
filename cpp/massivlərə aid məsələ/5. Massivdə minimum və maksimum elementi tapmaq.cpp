11f407ddd2468ea008384587a324a8ffdb073d04
Ismayil <149945594+ismayilqarayev@users.noreply.github.com>
2025-04-03 09:58:59 +0400

    massiv

---
+++ cpp/massivlərə aid məsələ/5. Massivdə minimum və maksimum elementi tapmaq.cpp
@@ -0,0 +1,8 @@
+5. Massivdə minimum və maksimum elementi tapmaq
+Təsvir: Verilən massivdə ən böyük və ən kiçik elementləri tapın.
+Giriş:
+n = 5
+Massiv: 7 2 9 3 6
+Çıxış:
+Ən kiçik ədəd: 2
+Ən böyük ədəd: 9
---
+++ cpp/massivlərə aid məsələ/7. Massivdə bir elementi axtarmaq.cpp
@@ -0,0 +1,10 @@
+7. Massivdə bir elementi axtarmaq
+Təsvir: İstifadəçi n ölçülü massiv və x ədədini daxil edir. Proqram x ədədinin massivdə olub-olmadığını yoxlamalıdır.
+
+Giriş:
+n = 5
+Massiv: 5 8 12 3 7
+Axtarılan ədəd: 12
+
+Çıxış:
+12 massivin içindədir.
