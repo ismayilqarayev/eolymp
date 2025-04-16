93c24111ff4aff75d5ce0d93128401316188d827
Ismayil <149945594+ismayilqarayev@users.noreply.github.com>
2025-04-16 08:19:31 +0400

    /

--- cpp/string məsələləri/19 Verilmiş cümlədə neçə cümlə olduğunu tapın. (Nöqtə ilə ayrılmış).cpp
+++ cpp/string məsələləri/19 Verilmiş cümlədə neçə cümlə olduğunu tapın. (Nöqtə ilə ayrılmış).cpp
@@ -1,3 +1,30 @@
 19 Verilmiş cümlədə neçə cümlə olduğunu tapın. (Nöqtə ilə ayrılmış)
 Giriş: "Salam. Mən gəlmişəm. Necəsən?"
 Çıxış: 3
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    string cumle;
+    cout << "Cumleni daxil edin: ";
+    getline(cin, cumle); // İstifadəçidən tam sətri oxuyuruq
+
+    int cumleSayi = 0;
+
+    // Sətirdəki hər bir simvola baxırıq
+    for (char simvol : cumle) 
+    {
+        // Əgər simvol cümlə sonu simvollarındandırsa, sayını artırırıq
+        if (simvol == '.' || simvol == '?' || simvol == '!') 
+        {
+            cumleSayi++;
+        }
+    }
+
+    cout << "Cumle sayi: " << cumleSayi << endl;
+
+    return 0;
+}
--- cpp/string məsələləri/20 Verilmiş sözün içində ’a’ hərfi neçə dəfə təkrarlanıb.cpp
+++ cpp/string məsələləri/20 Verilmiş sözün içində ’a’ hərfi neçə dəfə təkrarlanıb.cpp
@@ -1,3 +1,28 @@
 20 Verilmiş sözün içində ’a’ hərfi neçə dəfə təkrarlanıb?
 Giriş: "avtobus" Çıxış: 1
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    string soz;
+    int say = 0;
+
+    cout << "Sozu daxil edin: ";
+    cin >> soz;  // istifadəçidən sözü oxuyuruq
+
+    // hər simvolu yoxlayırıq
+    for (char herf : soz) 
+    {
+        if (herf == 'a' || herf == 'A') 
+        { // həm kiçik, həm böyük 'a' üçün
+            say++;
+        }
+    }
+
+    cout << "'a' herfi sayi: " << say << endl;
+
+    return 0;
+}
