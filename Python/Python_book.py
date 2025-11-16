fe5999e02cfbdfd9c2d763dbf288f7fb6e3081a4
Ismayil <149945594+ismayilqarayev@users.noreply.github.com>
2025-11-16 13:59:36 +0400

    Create Python_book.py

---
+++ Python/Python_book.py
@@ -0,0 +1,23 @@
+# Python dilində yazılmış proqram təlimatlar (instruksiyalar) 
+# toplusundan ibarətdir.
+# Hər bir təlimat yeni sətrdə yerləşdirilir.
+
+# Məsələn:
+
+print(2+3)
+print("Hello")
+
+# Python-da boşluqlar (indentlər) çox böyük rol oynayır.
+# Səhv qoyulmuş boşluq faktiki olaraq xəta hesab olunur.
+
+# Məsələn, aşağıdakı halda biz xəta alacağıq, baxmayaraq ki, kod əvvəlki nümunəyə çox oxşardır:
+
+print(2+3)
+   print("Hello")
+
+#  Ona görə də yeni təlimatları sətrin əvvəlində yerləşdirmək lazımdır.
+#  Bu, Python-un C# və ya Java kimi digər proqramlaşdırma 
+#  dillərindən əsas fərqlərindən biridir.
+
+if 1 < 2:
+    print("Hello")
