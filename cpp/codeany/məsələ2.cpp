169079c3567e78f4cf095adeebabacdff8a744a4
Ismayil <149945594+ismayilqarayev@users.noreply.github.com>
2025-11-29 20:39:26 +0400

    Create məsələ2.cpp

---
+++ cpp/codeany/məsələ2.cpp
@@ -0,0 +1,26 @@
+/*
+Hello World !
+Zaman limiti: 2,0 saniyə
+
+Yaddaş limiti: 128 MiB
+
+"Hello World!" setrini ekrana verin.
+
+Giriş verilənləri
+Giriş verilənləri yoxdur.
+
+Çıxış verilənləri
+Nümunədə verildiyi kimi "Hello World!" sətrini ekran verin.
+*/
+
+/*Həlli*/
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    // Ekrana "Hello World!" sətrini çıxarırıq
+    cout << "Hello World!";
+
+    return 0;
+}
