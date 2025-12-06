66989ff36361eff482af9b7163a2093ac7a56bcb
Ismayil <149945594+ismayilqarayev@users.noreply.github.com>
2025-12-06 19:38:54 +0400

    Create məsələ5.cpp

---
+++ cpp/codeany/məsələ5.cpp
@@ -0,0 +1,18 @@
+/*
+Professor və şarlar
+Zaman limiti: 1,0 saniyə
+
+Yaddaş limiti: 128 MiB
+
+
+Professor bayram üçün mavi, qırmızı və sarı şarlar alıb. Cəmi n ədəd. Sarı və mavi ilə birlikdə - a ədəd alıb. Qırmızı və mavi isə - b ədəd alıb.
+
+Professor neçə mavi, qırmızı və sarı şar almışdı?
+
+Giriş verilənləri
+Üç natural ədəd n, a, b.
+
+Çıxış verilənləri
+Bir sətirdə professorun aldığı mavi, qırmızı və sarı topların sayını çap edin.
+*/
+
