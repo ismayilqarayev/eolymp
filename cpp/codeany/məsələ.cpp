ffcf0d64c512d4bf1adaf6797066dac720a9b791
Ismayil <149945594+ismayilqarayev@users.noreply.github.com>
2025-12-06 20:37:52 +0400

    Create məsələ.cpp

---
+++ cpp/codeany/məsələ.cpp
@@ -0,0 +1,27 @@
+/*
+Stepanın dostları
+Zaman limiti: 1,0 saniyə
+
+Yaddaş limiti: 128 MiB
+
+Stepan məktəblilərin beynəlxalq proqramlaşdırma olimpiadasından (IOI) qayitdı və suvenir kimi 
+ müxtəlif rəngli daş gətirdi. Stepan xəsis oğlan deyil, buna görə də daşları dostları ilə bölüşmək qərarına gəldi. Hər bir dostuna bir ədəd daş verdi. Beləliklə, Stepanın özündə də bir ədəd daş qaldı. Onun dostlarının sayını təyin edin.
+
+Giriş verilənləri
+Yeganə tam n ( ) ədədi.
+
+Çıxış verilənləri
+Yeganə ədədi - Stepanın dostlarınn sayını verin.
+
+Qeydlər
+Nümünənin izahatı:
+Stepan 2 daş gətirdi, biri özündə qaldı. Deməli ikinci daşı yekanə dostuna verdi.
+
+Nümunələr
+Giriş verilənləri nümünəsi #1
+2
+
+Çıxış verilənləri nümünəsi #1
+1
+*/
+
