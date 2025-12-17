b8b53367fa5e9f50250fe106e6d1ca01aac187ea
Ismayil <149945594+ismayilqarayev@users.noreply.github.com>
2025-12-17 22:44:06 +0400

    /

--- Python/test.py
+++ Python/test.py
@@ -1,2 +1,10 @@
-# Konsola "Hello World" mesajının çıxarılması
-print("Hello World")
+def fun1():
+    num3 = [1,2,3,4]
+    num3.insert(4, 5)
+
+def fun2():
+    num4 = [1,2,3,4,5,6,7]
+    num4[2:2] = [3, 3]
+
+def fun3():
+    num
