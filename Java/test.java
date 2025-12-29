f4e0ea60f0786f48f75617c23c99bd6c03aeaf3c
Ismayil <149945594+ismayilqarayev@users.noreply.github.com>
2025-12-29 09:09:13 +0400

    ///

---
+++ Java/test.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+class Istifadeci{
+
+}
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        
+    }
+
+    public static int istifadeciSayiniOxu(){
+
+    }
+
+    public static void istifadecileriDaxilEt(int say){
+
+    }
+
+    
+}
+
