5231d23ea2b574f90a928e9d58664cf3387aea0a
Ismayil <149945594+ismayilqarayev@users.noreply.github.com>
2026-03-07 10:20:42 +0400

    /

--- cpp/mesele/5. Məsələ.cpp
+++ cpp/mesele/5. Məsələ.cpp
@@ -1,9 +1,9 @@
-5. Məsələ:
+/*5. Məsələ:
 Şərt: Bir tam ədəd 𝑦 verilmişdir. Əgər 𝑦 tək ədəd olarsa, ekrana “Tək ədəd” yazdırın.
 Əgər 𝑦 cüt ədəd olarsa, ekrana “Cüt ədəd” yazdırın.
 Giriş: 𝑦 = 9
 Çıxış: “Tək ədəd”
-
+*/
 #include <iostream>
 using namespace std;
 
