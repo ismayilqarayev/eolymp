7713a56ef160787862346b576046ddac962c071f
Ismayil Garayev <149945594+ismayilqarayev@users.noreply.github.com>
2026-03-23 10:10:29 +0400

    /

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-class Student {
-    private String ad;
+class Student{
+    String ad;
 
-    public Student(String ad) {
+    Student (String ad){
         this.ad = ad;
     }
 
@@ -21,16 +21,5 @@ class Student {
 }
 
 public class Main {
-    static void adDeyis(Student s, String yeniAd){
-        s.setAd(yeniAd);
-    }
-
-    public static void main(String[] args) {
-        Student s1 = new Student("Ismayil");
-
-        System.out.println("Deyishmeden evvel: " + s1.getAd());
-
-        adDeyis(s1, "Eli");
-        System.out.println("Deyishmeden sonra: " + s1.getAd());
-    }
+    
 }
