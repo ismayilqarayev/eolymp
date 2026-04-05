6b51a990cef2c8bfefdfd719059b147fb7d2d23a
Ismayil Garayev <149945594+ismayilqarayev@users.noreply.github.com>
2026-04-05 11:36:47 +0400

    Полиморфизм и динамическая диспетчеризация методов Абстрактные классы

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,4 +1,81 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+abstract class Student{
+    private String name;
+
+        public Student(String name){
+            this.name = name;
+        }
+
+        public String getName(){
+            return name;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
+
+    public abstract void showInfo();
+}
+
+class GradueateStudent extends Student{
+    private String University;
+
+    public GradueateStudent(String name, String University){
+        super(name);
+        this.University = University;
+    }
+
+    @Override
+    public void showInfo(){
+        System.out.println("Name: " + getName());
+        System.out.println("University: " + University);
+    }
+}
+
+class PhDStudent extends GradueateStudent{
+    private String researchTopic;
+    public PhDStudent(String name, String University, String researchTopic){
+        super(name, University);
+        this.researchTopic = researchTopic;
+    }
+
+    @Override
+    public void showInfo(){
+        super.showInfo();
+        System.out.println("Research Topic: " + researchTopic);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the name of the graduate student: ");
+        String GSName = scanner.nextLine();
+
+        System.out.print("Enter the university of the graduate student: ");
+        String GSUniversity = scanner.nextLine();
+
+        Student GS = new GradueateStudent(GSName, GSUniversity);
+        GS.showInfo();
+
+        System.out.print("Enter the name of the PhD student: ");
+        String PhDName = scanner.nextLine();
+
+        System.out.print("Enter the university of the PhD student: ");
+        String PhDUniversity = scanner.nextLine();
+
+        System.out.print("Enter the research topic of the PhD student: ");
+        String PhDResearchTopic = scanner.nextLine();
+
+        Student PhD = new PhDStudent(PhDName, PhDUniversity, PhDResearchTopic);
+        PhD.showInfo();
+
+        scanner.close();
+    }
+}
--- java/data.java
+++ java/data.java
@@ -702,3 +702,96 @@ public class Main {
         scanner.close();
     }
 }
+
+
+//--------------------------------------------
+// Abstract class Student
+// Bu sinif bir tələbənin ümumi xüsusiyyətlərini və davranışlarını təyin edir
+// Abstract olduğu üçün birbaşa obyekt yaradıla bilməz
+// Bu sinifdən törədilmiş siniflər öz spesifik xüsusiyyətlərini əlavə edə və showInfo metodunu implementasiya edə bilərlər
+// Bu, polymorphism və inheritance konseptlərini nümayiş etdirir
+// Student sinfi tələbənin adını saxlayır və showInfo metodunu abstrakt olaraq təyin edir
+// GraduateStudent sinfi Student sinifindən törədilir və universitet məlumatını əlavə edir
+// PhDStudent sinfi GraduateStudent sinifindən törədilir və tədqiqat
+// yeni daha spesifik məlumat əlavə edir (tədqiqat mövzusu)
+// yeni versiya yazdım daha aydın olsun deyə
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+abstract class Student{
+    private String name;
+
+        public Student(String name){
+            this.name = name;
+        }
+
+        public String getName(){
+            return name;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
+
+    public abstract void showInfo();
+}
+
+class GradueateStudent extends Student{
+    private String University;
+
+    public GradueateStudent(String name, String University){
+        super(name);
+        this.University = University;
+    }
+
+    @Override
+    public void showInfo(){
+        System.out.println("Name: " + getName());
+        System.out.println("University: " + University);
+    }
+}
+
+class PhDStudent extends GradueateStudent{
+    private String researchTopic;
+    public PhDStudent(String name, String University, String researchTopic){
+        super(name, University);
+        this.researchTopic = researchTopic;
+    }
+
+    @Override
+    public void showInfo(){
+        super.showInfo();
+        System.out.println("Research Topic: " + researchTopic);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the name of the graduate student: ");
+        String GSName = scanner.nextLine();
+
+        System.out.print("Enter the university of the graduate student: ");
+        String GSUniversity = scanner.nextLine();
+
+        Student GS = new GradueateStudent(GSName, GSUniversity);
+        GS.showInfo();
+
+        System.out.print("Enter the name of the PhD student: ");
+        String PhDName = scanner.nextLine();
+
+        System.out.print("Enter the university of the PhD student: ");
+        String PhDUniversity = scanner.nextLine();
+
+        System.out.print("Enter the research topic of the PhD student: ");
+        String PhDResearchTopic = scanner.nextLine();
+
+        Student PhD = new PhDStudent(PhDName, PhDUniversity, PhDResearchTopic);
+        PhD.showInfo();
+
+        scanner.close();
+    }
+}
