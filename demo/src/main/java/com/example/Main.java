6f297ae1a389c10e94824f36cab8439b29d07a1c
Ismayil Garayev <149945594+ismayilqarayev@users.noreply.github.com>
2026-04-11 19:05:24 +0400

    /

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -4,48 +4,50 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-abstract class Student{
+abstract class Student {
+
     private String name;
 
-        public Student(String name){
-            this.name = name;
-        }
+    public Student(String name) {
+        this.name = name;
+    }
 
-        public String getName(){
-            return name;
-        }
+    public String getName() {
+        return name;
+    }
 
-        public void setName(String name){
-            this.name = name;
-        }
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public abstract void showInfo();
 }
 
-class GradueateStudent extends Student{
+class GradueStudent extends Student {
     private String University;
 
-    public GradueateStudent(String name, String University){
+    public GradueStudent(String name, String University) {
         super(name);
         this.University = University;
     }
 
     @Override
-    public void showInfo(){
-        System.out.println("Name: " + getName());
+    public void showInfo() {
+        System.out.println("Graduate Student Name: " + getName());
         System.out.println("University: " + University);
     }
 }
 
-class PhDStudent extends GradueateStudent{
+class PhDStudent extends GradueStudent {
     private String researchTopic;
-    public PhDStudent(String name, String University, String researchTopic){
+
+    public PhDStudent(String name, String University, String researchTopic) {
         super(name, University);
         this.researchTopic = researchTopic;
     }
 
     @Override
-    public void showInfo(){
+    public void showInfo() {
         super.showInfo();
         System.out.println("Research Topic: " + researchTopic);
     }
@@ -61,8 +63,10 @@ public class Main {
         System.out.print("Enter the university of the graduate student: ");
         String GSUniversity = scanner.nextLine();
 
-        Student GS = new GradueateStudent(GSName, GSUniversity);
+        Student GS = new GradueStudent(GSName, GSUniversity);
         GS.showInfo();
+
+        // ----------------------------------------------
 
         System.out.print("Enter the name of the PhD student: ");
         String PhDName = scanner.nextLine();
