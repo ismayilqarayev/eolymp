ed2073b21b78a4487eb42665aa0ed049e65c2362
Ismayil Garayev <149945594+ismayilqarayev@users.noreply.github.com>
2026-04-18 13:12:34 +0400

    /

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -2,82 +2,20 @@ package com.example;
 
 import java.util.Scanner;
 
-abstract class Student{
-    private String name;
+class Student{
+    public String name;
+    public int age;
 
-    public Student(String name){
-        this.name = name;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    public void setName(String name){
-        this.name = name;
-    }
-
-    public abstract void showInfo();
-}
-
-class GratuateStudent extends Student{
-    private String University;
-
-    public GratuateStudent(String name, String University){
-        super(name);
-        this.University = University;
-    }
-
-    @Override
-    public void showInfo(){
-        System.out.println("Graduate Student Name: " + getName());
-        System.out.println("University: " + University);
-    }
-}
-
-class PhDStudent extends GratuateStudent{
-    private String researchTopic;
-
-    public PhDStudent(String name, String University, String researchString ){
-        super(name, University);
-        this.researchTopic = researchString;
-    }
-
-    @Override
-    public void showInfo(){
-        super.showInfo();
-        System.out.println("Research Topic: " + researchTopic);
-    }
-}
-
-public class Main{
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+        Student st = new Student();
 
-        System.out.print("Enter the name of the graduate student: ");
-        String GSName = scanner.nextLine();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter name:");
+        st.name = sc.nextLine();
+    }
 
-        System.out.print("Enter the university of the graduate student: ");
-        String GSUniversity = scanner.nextLine();
-
-        Student GS = new GratuateStudent(GSName, GSUniversity);
-
-        GS.showInfo();
-
-        // PhD student
-        System.out.print("Enter the name of the PhD student: ");
-        String PhDName = scanner.nextLine();
-
-        System.out.print("Enter the university of the PhD student: ");
-        String PhDUniversity = scanner.nextLine();
-
-        System.out.print("Enter the research topic of the PhD student: ");
-        String PhDResearchTopic = scanner.nextLine();
-
-        Student PhD = new PhDStudent(PhDName, PhDUniversity, PhDResearchTopic);
-        
-        PhD.showInfo();
-
-        scanner.close();
+    public void display(){
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
     }
 }
