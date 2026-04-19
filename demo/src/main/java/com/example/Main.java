2fa7cfe47be3d01e5c82dcabfbc9733fd047e755
Ismayil Garayev <149945594+ismayilqarayev@users.noreply.github.com>
2026-04-19 08:27:32 +0400

    /

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -3,15 +3,22 @@ package com.example;
 import java.util.Scanner;
 
 class Student{
+    
     public String name;
     public int age;
 
     public static void main(String[] args){
-        Student st = new Student();
 
+        Student st = new Student();
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter name:");
         st.name = sc.nextLine();
+
+        System.out.println("Enter age:");
+        st.age = sc.nextInt();
+
+        st.display();
     }
 
     public void display(){
