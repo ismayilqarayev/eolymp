9d926f3542b314e5d978f32c20441b20d3fc7c3f
Ismayil Garayev <149945594+ismayilqarayev@users.noreply.github.com>
2026-05-02 08:11:40 +0400

    /

--- Java_OOP_GUI/module-info.java
+++
@@ -1,3 +0,0 @@
-module StudentManagementSystem {
-    requires java.desktop;
-}
--- Java_OOP_GUI/run_gui.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting Java OOP Student Management GUI...
-cd /d "%~dp0"
-java StudentManagementGUI
-pause
