bbd4d5a2afa3edaa22f14a657860cd03ba1a8f6a
Ismayil Garayev <149945594+ismayilqarayev@users.noreply.github.com>
2026-05-02 10:22:55 +0400

    /

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,30 +1,5 @@
 package com.example;
 
-import java.util.Scanner;
-
-class Main {
-    public static int number_1;
-    public static int number_2;
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter a number_1:");
-        number_1 = scanner.nextInt();
-
-        System.out.println("Enter a number_2:");
-        number_2 = scanner.nextInt();
-
-        System.out.println("The sum of " + number_1 + " and " + number_2);
-
-        number_1 = number_2;
-        number_2 = number_1;
-
-        number_1++;
-        number_2++;
-
-        System.out.println("After incrementing, number_1 is: " + number_1);
-        System.out.println("After incrementing, number_2 is: " + number_2);
-
-        scanner.close();
-    }
+public class Main {
+    
 }
