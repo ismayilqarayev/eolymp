464687ea0b7fa5aeb73735619e7f86ac3235364a
Ismayil Garayev <149945594+ismayilqarayev@users.noreply.github.com>
2026-07-30 13:59:07 +0400

    /

--- cpp/Azercell_cup/movzu1_for_hell_cpp/movzu1_for_cpp/31_1_den_5_e_qeder_ededleri_cap_et.cpp
+++ cpp/Azercell_cup/movzu1_for_hell_cpp/movzu1_for_cpp/31_1_den_5_e_qeder_ededleri_cap_et.cpp
@@ -12,6 +12,13 @@
 using namespace std;
 int main(){
     int n; cin >> n;
-    for (int i = 1; i <= n; i++){ cout << i; if (i != n) cout << " "; }
+    for (int i = 1; i <= n; i++)
+    {
+         cout << i;
+        if (i != n)
+        {
+            cout << " ";
+        }
+    }
     cout << "\n";
 }
