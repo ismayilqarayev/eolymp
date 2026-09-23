a9e3999c3888d861ac57236d8d83cb52409def70
Ismayil <149945594+ismayilqarayev@users.noreply.github.com>
2026-09-23 08:40:23 +0400

    /

--- cpp/kodlar/test2.cpp
+++ cpp/kodlar/test2.cpp
@@ -24,3 +24,60 @@ int main()
     //cout << ad << soyad << yas << number << endl;
     cout << ad << " " << soyad << " " << yas << " " << number << endl;
 }
+//-------------------------------------------------------------
+// 2-ci test
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    char operation;
+
+    cout << "Birinci ededi daxil edin: ";
+    cin >> a;
+
+    cout << "Emeliyyati daxil edin (+, -, *, /, %): ";
+    cin >> operation;
+
+    cout << "Ikinci ededi daxil edin: ";
+    cin >> b;
+
+    if (operation == '+') 
+    {
+        cout << "Netice: " << a + b;
+    }
+    else if (operation == '-') 
+    {
+        cout << "Netice: " << a - b;
+    }
+    else if (operation == '*') 
+    {
+        cout << "Netice: " << a * b;
+    }
+    else if (operation == '/') 
+    {
+        if (b != 0) {
+            cout << "Netice: " << a / b;
+        }
+        else {
+            cout << "Xeta: sifira bolmek olmaz!";
+        }
+    }
+    else if (operation == '%') 
+    {
+        if (b != 0) 
+        {
+            cout << "Qaliq: " << a % b;
+        }
+        else 
+        {
+            cout << "Xeta: sifira bolmek olmaz!";
+        }
+    }
+    else 
+    {
+        cout << "Xeta: yanlis emeliyyat!";
+    }
+
+    return 0;
+}
